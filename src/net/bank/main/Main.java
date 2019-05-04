b63d874c54dd2348ff7bf2b70ff17ad2a826c9ec
Gero Lindner <43520235+privateY@users.noreply.github.com>
2019-05-04 21:50:41 +0200

    Added Config

--- src/net/bank/main/Main.java
+++ src/net/bank/main/Main.java
@@ -6,9 +6,17 @@ import net.bank.data.Data;
 
 public class Main extends JavaPlugin {
 	
+	private static Main plugin;
+	
 	public void onEnable() {
+		plugin = this;
+		
 		Data.log("§cBank System wurde gestartet!");
 		
+	}
+	
+	public static Main getMain() {
+		return plugin;
 	}
 
 }
