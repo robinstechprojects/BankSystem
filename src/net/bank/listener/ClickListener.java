0a35b45e746a5be54e854c696176ff35eb2a2472
Robin S <schrobin1505@gmail.com>
2019-05-04 22:14:50 +0200

    Click Listener
    Created Click-Listener

---
+++ src/net/bank/listener/ClickListener.java
@@ -0,0 +1,32 @@
+package net.bank.listener;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ClickListener implements Listener {
+	/**
+	 * 
+	 * Diese Klasse verwendet den Bukkit-Listener
+	 */
+
+	@EventHandler
+    public void onBlockClick(PlayerInteractEvent event) { //Aufruf wenn ein Spieler einen Block clickt
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {//War Klick ein Rechtsklick?
+            Player player = (Player) event.getPlayer();//event-Spieler in Variable speichern
+            Material clicked = event.getClickedBlock().getType();//geklicktes Material in Variable speichern
+            if(clicked == Material.BLUE_WOOL) {// Wenn geklickter Block blaue Wolle ist
+            	
+            }
+          
+
+
+            event.setCancelled(true);
+          
+        }
+    }
+	
+}
